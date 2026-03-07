6c18069a2f5ad81e3c25f88f466978adcdf2d3c8
한병현 <gksqudgus777@naver.com>
2026-03-07 15:40:07 +0900

    feature: Input 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import Input from "./components/Input";
+
 const App = () => {
-  return <></>;
+  const [isError, setIsError] = useState(false);
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    const v = e.target.value;
+    setValue(v);
+    if (v) setIsError(false);
+  };
+
+  const handleBlur = (e) => {
+    const v = e.target.value;
+    setValue(v);
+
+    if (!v) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+  };
+
+  return (
+    <>
+      <Input
+        value={value}
+        error={isError}
+        formType="from"
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+    </>
+  );
 };
 
 export default App;
