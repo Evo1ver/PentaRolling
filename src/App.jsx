f8c002879bcd47181892828241a4df7487fd6ceb
한병현 <gksqudgus777@naver.com>
2026-03-09 15:46:57 +0900

    chore: 컨벤션에 맞게 input 컴포넌트 폴더 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Input from "./components/Input";
+import Input from "./components/common/Input/Input";
 import Avatar from "./components/common/Avatar/Avatar";
 
 const App = () => {
