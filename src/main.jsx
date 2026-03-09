204453e83449c0cc2e45d6f0ff04d4c9f3c77774
Evo1ver <62330567+Evo1ver@users.noreply.github.com>
2026-03-09 19:42:10 +0900

    feat: modal context 전역적으로 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import ModalContextProvider from "./contexts/ModalProvider";
+import "./styles/reset.css";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
