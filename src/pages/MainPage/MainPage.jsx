55bac93f05376b34cdc79db79a61e20d9af77a0a
한병현 <gksqudgus777@naver.com>
2026-03-12 01:59:44 +0900

    feat: Main page 모바일 화면 구현

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,25 +1,50 @@
-import MainCardImage from "../../assets/images/main_page_card.png";
+import MainCardImage from "../../assets/images/main-page-card.png";
+import MainEmojiImage from "../../assets/images/main-page-emoji.png";
+import { Button } from "../../components/common/Button/Button";
 import * as S from "./MainPageStyle";
 
 const MainPage = () => {
   return (
-    <S.MainContainer>
-      <S.FirstSectionContainer>
-        <S.PointContainer>
-          <S.PointLabel>Point. 01</S.PointLabel>
-          <S.ScriptContainer>
-            <S.BoldScript>
-              누구나 손쉽게, 온라인 <S.ScriptBr />
-              롤링 페이퍼를 만들 수 있어요
-            </S.BoldScript>
-            <S.Script>로그인 없이 자유롭게 만들어요.</S.Script>
-          </S.ScriptContainer>
-        </S.PointContainer>
-        <S.MainCardImageWrapper>
-          <S.MainCardImage src={MainCardImage} />
-        </S.MainCardImageWrapper>
-      </S.FirstSectionContainer>
-    </S.MainContainer>
+    <>
+      <S.MainContainer>
+        <S.FirstSectionContainer>
+          <S.PointContainer>
+            <S.PointLabel>Point. 01</S.PointLabel>
+            <S.ScriptContainer>
+              <S.BoldScript>
+                누구나 손쉽게, 온라인 <S.FirstScriptBr />
+                롤링 페이퍼를 만들 수 있어요
+              </S.BoldScript>
+              <S.Script>로그인 없이 자유롭게 만들어요.</S.Script>
+            </S.ScriptContainer>
+          </S.PointContainer>
+          <S.MainCardImageWrapper>
+            <S.MainCardImage src={MainCardImage} />
+          </S.MainCardImageWrapper>
+        </S.FirstSectionContainer>
+        <S.SecondSectionContainer>
+          <S.PointContainer>
+            <S.PointLabel>Point. 02</S.PointLabel>
+            <S.ScriptContainer>
+              <S.BoldScript>
+                서로에게 이모지로 감정을
+                <S.SecondScriptBr />
+                표현해보세요
+              </S.BoldScript>
+              <S.Script>로그인 없이 자유롭게 만들어요.</S.Script>
+            </S.ScriptContainer>
+          </S.PointContainer>
+          <S.MainCardImageWrapper>
+            <S.MainCardImage src={MainEmojiImage} />
+          </S.MainCardImageWrapper>
+        </S.SecondSectionContainer>
+      </S.MainContainer>
+      <S.ButtonWrapper>
+        <Button variant="primary" size="large">
+          롤링 페이퍼 만들기
+        </Button>
+      </S.ButtonWrapper>
+    </>
   );
 };
 
--- src/pages/MainPage/MainPageStyle.js
+++ src/pages/MainPage/MainPageStyle.js
@@ -31,6 +31,7 @@ export const PointLabel = styled.span`
   border-radius: 50px;
   background-color: #95cce4;
   font: var(--font-14-bold);
+  font-weight: 600;
   color: var(--white);
 `;
 
@@ -44,13 +45,18 @@ export const BoldScript = styled.h2`
   font: var(--font-18-bold);
   color: var(--gray-900);
 `;
+
 export const Script = styled.p`
   font: var(--font-15-regular);
   color: var(--gray-500);
 `;
 
-export const ScriptBr = styled.br`
+export const FirstScriptBr = styled.br`
   display: none;
+`;
+
+export const SecondScriptBr = styled.br`
+  display: block;
 `;
 
 export const MainCardImageWrapper = styled.div`
@@ -63,4 +69,21 @@ export const MainCardImage = styled.img`
   width: 380px;
   height: 100px;
   flex: 0 0 auto;
+`;
+
+export const SecondSectionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  padding: 24px 24px 43px;
+
+  border-radius: 20px;
+  background-color: var(--surface);
+  overflow: hidden;
+`;
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 51px auto 24px;
+  width: 100%;
 `;
