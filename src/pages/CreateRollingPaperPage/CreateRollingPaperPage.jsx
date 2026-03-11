f02cbd9832cf0789a8da6fa4c00f0a946b40c564
Evo1ver <62330567+Evo1ver@users.noreply.github.com>
2026-03-11 19:36:27 +0900

    feat: 배경색 설정 부분 및 조건부 button 활성화 구현 완료

--- src/pages/CreateRollingPaperPage/CreateRollingPaperPage.jsx
+++ src/pages/CreateRollingPaperPage/CreateRollingPaperPage.jsx
@@ -1,14 +1,51 @@
 import { useState } from "react";
 import Input from "../../components/common/Input/Input";
 import * as S from "./CreateRollingPaperPageStyle";
+import {
+  Button,
+  ToggleButtonGroup,
+} from "../../components/common/Button/Button";
+import selectedIcon from "../../assets/icons/selected.svg";
+import { useNavigate } from "react-router-dom";
+
+const toggleOption = [
+  {
+    value: "color",
+    label: "컬러",
+  },
+  {
+    value: "image",
+    label: "이미지",
+  },
+];
+
+const BACKGROUND_COLORS = ["#FFE2AD", "#ECD9FF", "#B1E4FF", "#D0F5C3"];
 
 const CreateRollingPaperPage = () => {
   const [recipient, setRecipient] = useState("");
   const [inputError, setInputError] = useState(false);
+  const [toggle, setToggle] = useState(toggleOption[0].value);
+  const [selectedFragment, setSelectedFragment] = useState(0);
+  const navigate = useNavigate();
+
+  const handleOptionChange = (value) => {
+    setToggle(value);
+  };
+
+  const handleFragmentClick = (index) => {
+    setSelectedFragment(index);
+  };
+
+  const handleSubmit = () => {
+    // 폼 제출
+
+    // 생성 완료 시 생성된 페이지 이동
+    navigate(`/post/`);
+  };
 
   return (
     <>
-      <S.Form action="/create">
+      <S.Form onSubmit={handleSubmit}>
         <Input
           value={recipient}
           error={inputError}
@@ -16,6 +53,49 @@ const CreateRollingPaperPage = () => {
           onChange={(e) => setRecipient(e.target.value)}
           onBlur={() => setInputError(!recipient)}
         />
+
+        <div>
+          <S.SelectorDescription>
+            <S.SelectorLabel>배경화면을 선택해 주세요.</S.SelectorLabel>
+            컬러를 선택하거나 이미지를 선택할 수 있습니다.
+          </S.SelectorDescription>
+          <div style={{ marginBottom: "40px" }}>
+            <ToggleButtonGroup
+              options={toggleOption}
+              onChange={handleOptionChange}
+            />
+          </div>
+          <S.SelectorContent>
+            {toggle === "color" ? (
+              <>
+                {BACKGROUND_COLORS.map((color, index) => (
+                  <S.SelectorFragment
+                    key={color}
+                    $color={color}
+                    onClick={() => handleFragmentClick(index)}
+                    $isActive={selectedFragment === index}
+                  >
+                    {index === selectedFragment && (
+                      <>
+                        <img src={selectedIcon} alt="" />
+                      </>
+                    )}
+                  </S.SelectorFragment>
+                ))}
+              </>
+            ) : (
+              <div>이미지</div>
+            )}
+          </S.SelectorContent>
+        </div>
+
+        <Button
+          state={!recipient ? "disabled" : "default"}
+          size={56}
+          style={{ width: "100%" }}
+        >
+          생성하기
+        </Button>
       </S.Form>
     </>
   );
--- src/pages/CreateRollingPaperPage/CreateRollingPaperPageStyle.js
+++ src/pages/CreateRollingPaperPage/CreateRollingPaperPageStyle.js
@@ -1,9 +1,56 @@
 import styled from "styled-components";
 
+const MOBILE_BREAKPOINT = "361px"; // 360px 초과 시 적용
+
 export const Form = styled.form`
   padding: 50px 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 50px;
+
+  & > div {
+    width: 100%;
+  }
+
+  @media (min-width: ${MOBILE_BREAKPOINT}) {
+    width: 100%;
+    max-width: 720px;
+  }
+`;
+
+export const SelectorDescription = styled.div`
+  font: var(--font-16-regular);
+  color: var(--gray-500);
+  margin-bottom: 24px;
+`;
+
+export const SelectorLabel = styled.div`
+  font: var(--font-24-bold);
+  color: var(--gray-900);
+  margin-bottom: 4px;
+`;
+
+export const SelectorContent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+
+  @media (min-width: ${MOBILE_BREAKPOINT}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+export const SelectorFragment = styled.div`
+  background-color: ${({ $color }) => $color};
+  ${({ $imageURL }) => $imageURL && `background-image: url(${$imageURL})`};
+  width: 100%;
+  height: 150px;
+  border: rgba(0, 0, 0, 0.08);
+
+  ${({ $isActive, $imageURL }) => $isActive && $imageURL && `opacity: 0.3`};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
