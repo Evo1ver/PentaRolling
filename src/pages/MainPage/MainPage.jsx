b4ed2f2475dce174ffeb9c57772eecb0af148c22
한병현 <gksqudgus777@naver.com>
2026-03-11 19:00:49 +0900

    feat: Main page FirstSection 모바일 구현

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,26 @@
+import MainCardImage from "../../assets/images/main_page_card.png";
+import * as S from "./MainPageStyle";
+
 const MainPage = () => {
-  return <h1>임시 메인 페이지 입니다.</h1>;
+  return (
+    <S.MainContainer>
+      <S.FirstSectionContainer>
+        <S.PointContainer>
+          <S.PointLabel>Point. 01</S.PointLabel>
+          <S.ScriptContainer>
+            <S.BoldScript>
+              누구나 손쉽게, 온라인 <S.ScriptBr />
+              롤링 페이퍼를 만들 수 있어요
+            </S.BoldScript>
+            <S.Script>로그인 없이 자유롭게 만들어요.</S.Script>
+          </S.ScriptContainer>
+        </S.PointContainer>
+        <S.MainCardImageWrapper>
+          <S.MainCardImage src={MainCardImage} />
+        </S.MainCardImageWrapper>
+      </S.FirstSectionContainer>
+    </S.MainContainer>
+  );
 };
 
 export default MainPage;
---
+++ src/pages/MainPage/MainPageStyle.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin: 42px 24px;
+`;
+
+export const FirstSectionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 29px;
+  padding: 24px;
+
+  border-radius: 20px;
+  background-color: var(--surface);
+  overflow: hidden;
+`;
+
+export const PointContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const PointLabel = styled.span`
+  padding: 4px 12px;
+  width: 82px;
+  height: 28px;
+  border-radius: 50px;
+  background-color: #95cce4;
+  font: var(--font-14-bold);
+  color: var(--white);
+`;
+
+export const ScriptContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const BoldScript = styled.h2`
+  font: var(--font-18-bold);
+  color: var(--gray-900);
+`;
+export const Script = styled.p`
+  font: var(--font-15-regular);
+  color: var(--gray-500);
+`;
+
+export const ScriptBr = styled.br`
+  display: none;
+`;
+
+export const MainCardImageWrapper = styled.div`
+  margin: 0 -24px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const MainCardImage = styled.img`
+  width: 380px;
+  height: 100px;
+  flex: 0 0 auto;
+`;
