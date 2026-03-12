e428123f4d4ac3b813bea79e1c99b888357ba7db
DAEUN KWON <dadax0715@gmail.com>
2026-03-12 23:18:52 +0900

    chore: React Router 기본 설정 및 App.jsx 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,12 @@
-import CardList from "./components/CardList/CardList";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  const mockAvatars = [
-    "https://learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com/sprint-proj-image/default_avatar.png",
-    "https://picsum.photos/id/522/100/100",
-    "https://picsum.photos/id/547/100/100",
-    "https://picsum.photos/id/268/100/100",
-  ];
-
-  const mockReactions = [
-    { id: 1, emoji: "👍", count: 20 },
-    { id: 2, emoji: "😍", count: 12 },
-    { id: 3, emoji: "😢", count: 7 },
-  ];
-
+function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "40px",
-        padding: "40px",
-        backgroundColor: "#f5f5f5",
-        minHeight: "100vh",
-      }}
-    >
-      <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-        <CardList
-          recipientName="SoWon"
-          backgroundColor="green"
-          avatarImageUrls={mockAvatars}
-          messageCount={30}
-          reactions={mockReactions}
-        />
-
-        <CardList
-          recipientName="Jisoo"
-          backgroundImageUrl="https://picsum.photos/id/683/3840/2160"
-          avatarImageUrls={mockAvatars}
-          messageCount={7}
-          reactions={mockReactions}
-        />
-
-        <CardList
-          recipientName="Daeun"
-          backgroundColor="beige"
-          messageCount={0}
-        />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
