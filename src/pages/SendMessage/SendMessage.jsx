6424b079a680dbff2de4f26dc4b957533071cc40
gnath <rudqor1618@naver.com>
2026-03-12 14:41:50 +0900

    refactor: SendMessage 가독성 개선

--- src/pages/SendMessage/SendMessage.jsx
+++ src/pages/SendMessage/SendMessage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import Input from "../../components/common/Input/Input";
+import Avatar from "../../components/common/Avatar/Avatar";
 import AvatarSelector from "../../components/common/Avatar/AvatarSelector";
 import { Dropdown } from "../../components/common/Dropdown/Dropdown";
 import Textarea from "../../components/common/Textarea/Textarea";
@@ -17,6 +18,9 @@ import * as S from "./SendMessageStyle";
 const SendMessage = () => {
   const { recipientId } = useParams();
   const navigate = useNavigate();
+  const handleSuccess = () => navigate(`/recipients/${recipientId}`);
+
+  // 모바일: small(40px), 태블릿 이상: medium(56px)
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   useEffect(() => {
@@ -42,7 +46,7 @@ const SendMessage = () => {
     handleContentChange,
     handleFontChange,
     handleSubmit,
-  } = useMessageForm(recipientId, () => navigate(`/recipients/${recipientId}`));
+  } = useMessageForm(recipientId, handleSuccess);
 
   return (
     <S.PageWrapper $backgroundColor={backgroundColor}>
@@ -60,7 +64,7 @@ const SendMessage = () => {
         <S.Section>
           <S.SectionTitle>프로필 이미지</S.SectionTitle>
           <S.ProfileImageRow>
-            <S.ProfileAvatar src={form.profileImage} size="large" />
+            <Avatar src={form.profileImage} size="large" />
             <S.AvatarSelectorWrapper>
               <S.SelectorGuide>프로필 이미지를 선택해주세요!</S.SelectorGuide>
               {isLoadingImages ? (
