bd6a0f0eb4ef1d91dd7bb19456da837fcc4c62a5
한병현 <gksqudgus777@naver.com>
2026-03-14 17:34:56 +0900

    refactor: Header 유무에 따른 margin-top 조절

--- src/pages/CreateRollingPaperPage/CreateRollingPaperPageStyle.js
+++ src/pages/CreateRollingPaperPage/CreateRollingPaperPageStyle.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const TABLET_BREAKPOINT = "768px"; // 768px 이상 시 적용
+const MOBILE_BREAKPOINT = "360px";
 
 export const Form = styled.form`
-  margin-top: 64px;
   padding: 50px 20px;
   display: flex;
   flex-direction: column;
@@ -13,6 +13,10 @@ export const Form = styled.form`
 
   & > div {
     width: 100%;
+  }
+
+  @media (min-width: ${MOBILE_BREAKPOINT}) {
+    margin-top: 64px;
   }
 
   @media (min-width: ${TABLET_BREAKPOINT}) {
