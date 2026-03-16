82df4251ca3636a4ddd6b38442898bda06192678
gnath12 <87157783+gnath12@users.noreply.github.com>
2026-03-16 16:11:12 +0900

    chore: App.jsx에 import 추가
    - PostMessagePage, SendMessagePage import 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastsContextProvider } from "./contexts/ToastsProvider";
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage/MainPage";
 import CreateRollingPaperPage from "./pages/CreateRollingPaperPage/CreateRollingPaperPage";
+import PostMessage from "./pages/PostMessage/PostMessage";
+import SendMessage from "./pages/SendMessage/SendMessage";
 import APITest from "./pages/Test/APITest";
 
 const App = () => {
   return (
-    <>
+    <ToastsContextProvider>
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/post" element={<CreateRollingPaperPage />} />
+        <Route path="/post/:id" element={<PostMessage />} />
+        <Route path="/post/:id/message" element={<SendMessage />} />
         <Route path="/test" element={<APITest />} />
       </Routes>
-    </>
+    </ToastsContextProvider>
   );
 };
 
